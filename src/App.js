47bdefae4dc1813b983719855298dfa92f2066f0
Alisha <alisha@gmail.com>
2024-07-03 22:44:56 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 // src/App.js
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [updateAvailable, setUpdateAvailable] = useState(false);
-
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.addEventListener('updatefound', () => {
-          const installingWorker = registration.installing;
-          if (installingWorker) {
-            installingWorker.addEventListener('statechange', () => {
-              if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                setUpdateAvailable(true);
-              }
-            });
-          }
-        });
-      });
-    }
-  }, []);
-
-  const handleUpdate = () => {
-    navigator.serviceWorker.getRegistration().then((registration) => {
+    const checkForUpdates = async () => {
+      const registration = await navigator.serviceWorker.getRegistration();
       if (registration && registration.waiting) {
         registration.waiting.postMessage({ type: 'SKIP_WAITING' });
         registration.waiting.addEventListener('statechange', (event) => {
@@ -33,19 +15,19 @@ function App() {
           }
         });
       }
-    });
-  };
+    };
+
+    window.addEventListener('load', checkForUpdates);
+
+    return () => window.removeEventListener('load', checkForUpdates);
+  }, []);
 
   return (
     <div className="App">
-      {updateAvailable && (
-        <div className="update-notification">
-          <p>New update is available.</p>
-          <button onClick={handleUpdate()}>Update Now</button>
-        </div>
-      )}
-      data
-      <h1>data</h1>
+      <header className="App-header">
+        dataimage2
+        dataimage3
+      </header>
     </div>
   );
 }
