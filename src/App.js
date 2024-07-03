45e5045e9e802869884ac082e048c1110af408ba
Alisha <alisha@gmail.com>
2024-07-03 21:49:34 +0530

    caching

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const checkForUpdates = async () => {
+      const registration = await navigator.serviceWorker.getRegistration();
+      if (registration && registration.waiting) {
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+        registration.waiting.addEventListener('statechange', (event) => {
+          if (event.target.state === 'activated') {
+            window.location.reload();
+          }
+        });
+      }
+    };
+
+    window.addEventListener('load', checkForUpdates);
+
+    return () => window.removeEventListener('load', checkForUpdates);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        dataimage2
       </header>
     </div>
   );
